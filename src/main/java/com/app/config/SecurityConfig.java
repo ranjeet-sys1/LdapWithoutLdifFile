d64c7ce5f007a7b4e03a94e5410face9b614cf29
ranjeet.suman <ranjeetsuman123@gmail.com>
2022-01-06 20:44:25 +0530

    ldap authenticated with Jwt response

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -4,10 +4,21 @@
 package com.app.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author ranjeet.suman
@@ -36,21 +47,19 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter   {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().disable().csrf().disable();
         http
         	.authorizeRequests()
-        		.antMatchers("/login**").permitAll()
-        		.antMatchers("/profile/**").fullyAuthenticated()
+        		.antMatchers("/login").authenticated()
+        		.antMatchers("/profile/**").authenticated()
             	.antMatchers("/").permitAll()
-            	.and()
-            .formLogin()
-            	.loginPage("/login")
-            	.failureUrl("/login?error")
-            	.permitAll()
-            	.and()
-            .logout()
-            	.invalidateHttpSession(true)
-            	.deleteCookies("JSESSIONID")
-            	.permitAll();
+				.anyRequest().fullyAuthenticated()
+				.and()
+				.httpBasic();
+
+
+
+
 	}
 	
 	
@@ -74,5 +83,28 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter   {
 	            .withUser("admin").password("admin").roles("ADMIN");
 		}
 	}
+	@Bean
+	public SimpleUrlAuthenticationFailureHandler failureHandler() {
+		return new SimpleUrlAuthenticationFailureHandler() {
+			@Override
+			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+				response.sendError(401, "Authentication failure");
+			}
+		};
+	}
 
+	@Bean
+	public SimpleUrlAuthenticationSuccessHandler successHandler() {
+		return new SimpleUrlAuthenticationSuccessHandler() {
+			@Override
+			public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+				response.setStatus(200);
+			}
+		};
+	}
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 }
---
+++ src/main/java/com/app/utility/ResponseConstantIntegerValue.java
@@ -0,0 +1,10 @@
+package com.app.utility;
+
+public class ResponseConstantIntegerValue {
+    public static final Integer SUCCESS_RESPONSE = 200;
+    public static final Integer FAILURE_RESPONSE = 400;
+    public static final Integer UNAUTHORIZED_RESPONSE = 401;
+    public static final Integer  REQUEST_FORBIDDEN= 403;
+
+
+}
