eb83e7ff1cf29d419cb633000fb3ac9a37150e23
ranjeet.suman <ranjeetsuman123@gmail.com>
2022-01-06 20:44:25 +0530

    ldap authenticated with Jwt response

---
+++ src/main/java/com/app/utility/ResponseConstantValue.java
@@ -0,0 +1,11 @@
+package com.app.utility;
+
+public class ResponseConstantValue {
+    public static final String SUCCESS_MESSAGE= "successFully Login";
+    public static final String FAILURE_MESSAGE = "Failed to Login";
+    public static final String INTERNAL_ERROR = "internal_error";
+    public static final String UNAUTHORIZED_RESPONSE = "Unauthorized response status";
+    public static final String REQUEST_FORBIDDEN = "access to the requested resource is forbidden";
+
+
+}
